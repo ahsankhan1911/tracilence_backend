a32b79660e94a1a31c71889ea581c337df1aed97
Invision-043 <muhammad.hani@invisionsolutions.ca>
2019-02-20 21:04:15 +0500

    build added

---
+++ client/build/precache-manifest.1d6b1a00e6450d184b50d4c86af64380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "385e424a15a00aedce40",
+    "url": "/static/js/main.385e424a.chunk.js"
+  },
+  {
+    "revision": "682a75770a6a39495ba5",
+    "url": "/static/js/1.682a7577.chunk.js"
+  },
+  {
+    "revision": "385e424a15a00aedce40",
+    "url": "/static/css/main.cf57085a.chunk.css"
+  },
+  {
+    "revision": "ed1ed732a38826acd5ca659ffe28b526",
+    "url": "/index.html"
+  }
+];
