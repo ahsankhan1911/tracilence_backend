77af0ac9ea0c9ecfccbd4ff8538c751d820ba2ef
Invision-043 <muhammad.hani@invisionsolutions.ca>
2019-03-12 18:36:43 +0500

    cors bug fixed

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.use('/images',express.static('./client/public/images'))
 
 //CORS congif
 app.use((req, res, next) =>{
-  res.header("Access-Control-Allow-Origin", "");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
